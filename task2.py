93f900df6f5d7b1ba77ce2300a7fc7fbd1a285e4
jaykharat13-cpu <jaykharat13@gmail.com>
2026-02-23 18:57:23 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -3,4 +3,5 @@
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
 full_name = first_name + " " + last_name
-print("Hello,", full_name + "! Welcome aboard.")
+print("Hello,", full_name + "! Welcome to Python programe .")
+
